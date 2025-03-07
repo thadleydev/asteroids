998ed14a750eb23c19240adf2a357ca386d21673
thadleydev <18191317+thadleydev@users.noreply.github.com>
2025-03-06 23:21:50 -0500

    added 60fps

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,6 +20,7 @@ def main():
         color = pygame.Color(0, 0, 0)
         pygame.Surface.fill(screen, color)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
